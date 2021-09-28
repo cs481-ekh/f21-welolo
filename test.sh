de00d02c7bc1f8b97ab6b48580b0fae8b94bb520
ZahraRahmani2 <zahrarahmani@u.boisestate.edu>
2021-09-28 11:42:56 -0600

    test.sh updated

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 FILE=./Welolo/client/src/App.test.js
 if test -f "$FILE"; then
     echo "Yes it exists"
-    exit 127
+    exit 0
 else
     echo "Error: no test specified" 
-    exit 0
+    exit 127
 fi
