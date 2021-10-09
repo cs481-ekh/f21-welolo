1517ad85243dc13e93d1061e93ff5dc69258d479
Xavier Frehner <xavierfrehner@Xaviers-MacBook-Pro.local>
2021-10-09 16:28:27 -0600

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,6 @@ if [ -d "./Welolo/node_modules" ]
 then
     rm -rf ./Welolo/node_modules
     cd Welolo
-<<<<<<< Updated upstream
-    npm install mysql express 
-    exit 0
-else 
-    cd Welolo
-    npm install mysql express
-=======
     npm install
     npm install mysql express twilio
     npm install cypress --save -dev
@@ -23,7 +16,6 @@ else
     npm install mysql twilio express
     cd client
     npm install express
->>>>>>> Stashed changes
     exit 0
 fi
 exit 127
