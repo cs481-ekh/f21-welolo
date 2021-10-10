febfc51c4f78d9bc848277b88c10dc07ae043ee6
Xavier Frehner <xavierfrehner@Xaviers-MacBook-Pro.local>
2021-10-10 11:47:17 -0600

    Forgot somethin

--- Welolo/client/src/index.js
+++ Welolo/client/src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ErrorMessage from './error_message'
-<<<<<<< Updated upstream
 import SuccessfulPayment from './SuccessfulPayment.js';
-=======
-import SuccessfulPayment from './pages/SuccessfulPayment';
-import SMS_Sender from './components/SMS_Sender';
->>>>>>> Stashed changes
 
 
 class PaymentForm extends React.Component {
