5a629623c54fb18799ecc4c3c88cb237b89d13b7
Alyssa West <alyssarosewest@hotmail.com>
2021-10-10 01:02:48 -0600

    Started integrating Cypress into test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 FILE=./Welolo/client/src/App.test.js
 if test -f "$FILE"; then
-    cd ~
+    cd ..
+    cd ..
+    cd ..
     npx cypress run
 else
     echo "Error: no test specified" 
