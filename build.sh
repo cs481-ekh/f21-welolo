ec6b2037cee7358ef4fed71334e4fcd311931cc0
Alyssa West <alyssarosewest@hotmail.com>
2021-10-10 16:18:49 -0600

    Still trying to get servers working

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ then
     npm install mysql express 
 
     echo "Starting Node Server"
-    (npm start&)
+    npm start&
 
     echo "Starting React Server"
     cd ./Welolo/client/
     npm start & npx wait-on http://localhost:3000
-    (npm start&)
+    npm start&
 
     exit 0
 else 
