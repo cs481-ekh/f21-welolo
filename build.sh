acd9aea60fe3a64ffa9813da533fe6f20c7b3765
Alyssa West <alyssarosewest@hotmail.com>
2021-10-10 15:55:25 -0600

    Still trying to get servers running properly...

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@ then
     rm -rf ./Welolo/node_modules
     cd Welolo
     npm install mysql express 
+
+    echo "Starting Node Server"
+    npm start & npx wait-on http://localhost:3001
+
+    echo "Starting React Server"
+    cd ./Welolo/client/
+    npm start & npx wait-on http://localhost:3000
+    
     exit 0
 else 
     cd Welolo
