f7049adfed5fa8d90790b140016b889b6d3a6ac0
xfrehner12 <44218062+xfrehner12@users.noreply.github.com>
2021-10-11 23:16:55 -0600

    Closes #23 (#39)

--- Welolo/client/src/index.js
+++ Welolo/client/src/index.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ErrorMessage from './error_message'
-import SuccessfulPayment from './pages/SuccessfulPayment';
+import SuccessfulPayment from './SuccessfulPayment.js';
+import SMS_SENDER from './components/SMS_SENDER.js';
 
 
 class PaymentForm extends React.Component {
   render() {
     return (
-      <form>
-        <h1>Payment</h1>
-        <p>Recipient Name:</p>
-        <input
-          type="text" data-cy="recipient_name"
-        />
-        <p>Recipient Ph#:</p>
-        <input
-          type="text" data-cy="recipient_ph"
-        />
-        <p>Quantity of funds:</p>
-        <input
-          type="text" data-cy="fund_quantity"
-        />
-        <p>Message:</p>
-        <input
-          type="text" data-cy="message"
-        />
-        <input
-        type='submit' value='Pay forward' data-cy="submit"/>
+      <div>
+        <br/>
+        <SMS_SENDER />
         <ErrorMessage />
         <SuccessfulPayment />
-      </form>
+      </div>
 
     )}
 }
