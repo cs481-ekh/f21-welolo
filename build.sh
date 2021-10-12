eb6004d1eed33847d9e213382ffd11f352474c6c
Alyssa West <alyssarosewest@hotmail.com>
2021-10-11 19:12:08 -0600

    testing to see if build fails if a command fails

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ then
     npm install concurrently -g concurrently
 
     echo "Starting Servers"
-    concurrently "./start_node_server.sh" "./start_react_server.sh"
+    concurrently "./start_node_server.sh" "./start_react_serversh"
 
     exit 0
 else 
