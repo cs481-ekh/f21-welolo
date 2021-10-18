def323d495bf8846c133275c67604bccb041e1a4
Xavier Frehner <xavierfrehner@Xaviers-MacBook-Pro.local>
2021-10-17 20:32:42 -0600

    One change to stopping point

--- Welolo/client/src/index.js
+++ Welolo/client/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SuccessfulPayment from './pages/SuccessfulPayment.js';
 import TRANSACTION_INIT from './components/TRANSACTION_INIT.js';
-import Form from './components/Form.js';
 
-const myForm = new Form();
 class PaymentForm extends React.Component {
   render() {
     return (
