52dc17296e3d990841b3de9e1989c0394dcea8b4
ZahraRahmani2 <zahrarahmani@u.boisestate.edu>
2021-10-21 16:10:07 -0600

    tests for payment and sms endpoints #50

--- Welolo/cypress/integration/sms_endpoints.js
+++ Welolo/cypress/integration/sms_endpoints.js
@@ -1,3 +1,4 @@
+
 describe("Testing SMS API Endpoints", () => {
     it("Testing GET test endpoint", () => {
         cy.request("http://localhost:3001/test")
@@ -5,4 +6,21 @@ describe("Testing SMS API Endpoints", () => {
                     expect(response.body).to.have.property('message', 'This endpoint is working properly!');
             })
     })
+
+    it("Testing POST api/send_message endpoint",()=>{
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/api/send_message',
+            form: true, 
+            body: {
+                recipient_name: "john",
+                recipient:'+15005550006',
+                sender_quantity:'100',
+                body:'Testing'
+            },
+          }).then((response) => {
+            expect(response.body).to.have.property('success', true)
+        })
+    })
+    
 })
