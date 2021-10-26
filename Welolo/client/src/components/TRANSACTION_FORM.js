aa25c36cbc8dce41bcbb3ce1cad92cf2b4d74abb
Alyssa West <alyssarosewest@hotmail.com>
2021-10-25 19:32:13 -0600

    Removed console log statements used in testing

--- Welolo/client/src/components/TRANSACTION_FORM.js
+++ Welolo/client/src/components/TRANSACTION_FORM.js
@@ -125,7 +125,6 @@ class TRANSACTION_FORM extends Component {
             .then(data => { return data.success })
             .catch(err => {
                 console.log(err)
-                console.log("Failure");
                 return false
             })
         if(successfulPayment) {
