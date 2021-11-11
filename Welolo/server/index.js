f7b6dcf4fc0be53bcb22d1680645aa17d55c77d7
Alyssa West <alyssarosewest@hotmail.com>
2021-11-10 19:07:15 -0700

    Fixed type issue causing gravity modal not to appear on merchant product selection

--- Welolo/server/index.js
+++ Welolo/server/index.js
@@ -134,14 +134,12 @@ app.get("/api/merchants", (req,res) => {
 
 app.get("/test", (req, res) => {
   res.json({ message: "This endpoint is working properly!" });
-  //TODO: Replace with our own functionality and change endpoint name
 });
 
 app.get("/payment_successful", (req, res) => {
   res.json({message: "success"});
 });
 
-//TODO: Add other endpoints here
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
